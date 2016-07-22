ffbed8fc157551f773fdc4a104aac1e78f502624
MonishaPillai_10 <monisha.pillai@cennest.com>
2016-07-22 11:36:01 +0530

    Code for converting JSON data to csv, PNG

--- app/org/shared/index.ts
+++ app/org/shared/index.ts
@@ -1,4 +1,5 @@
 export * from "./models/orgchart.model";
+export * from "./models/orgnode.base.model";
 export * from "./models/orgnode.model";
 export * from "./org.service"
 export * from "./models/orgchart.modes";
