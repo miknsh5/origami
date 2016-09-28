d1b17712d7e26d5cc494ee8a3130282669a2f208
Nilesh-Bhor <nilesh-bhor@github.com>
2016-09-28 15:09:32 +0530

    removed dublicate css styles

--- app/org/json-to-csv/json-to-csv.component.ts
+++ app/org/json-to-csv/json-to-csv.component.ts
@@ -6,7 +6,7 @@ import { CSVConversionHelper } from "../shared/csv-helper";
 @Component({
     selector: "sg-origami-csv",
     templateUrl: "app/org/json-to-csv/json-to-csv.component.html",
-    styleUrls: ["app/org/json-to-csv/json-to-csv.component.css", "app/style.css"],
+    styleUrls: ["app/org/json-to-csv/json-to-csv.component.css"],
     providers: [CSVConversionHelper]
 })
 
--- app/org/org-node-detail/org-node-detail.component.ts
+++ app/org/org-node-detail/org-node-detail.component.ts
@@ -6,7 +6,7 @@ import { OrgNodeModel, OrgService } from "../shared/index";
 @Component({
     selector: "sg-org-node-detail",
     templateUrl: "app/org/org-node-detail/org-node-detail.component.html",
-    styleUrls: ["app/org/org-node-detail/org-node-detail.component.css", "app/style.css"]
+    styleUrls: ["app/org/org-node-detail/org-node-detail.component.css"]
 })
 
 export class OrgNodeDetailComponent implements OnChanges, AfterContentChecked {
