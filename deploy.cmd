427e801b00cd00e21b5ba523c4217050d997b04e
Nilesh-Bhor <nilesh-bhor@github.com>
2016-10-05 18:38:07 +0530

    added webpack build command in azure deploymentscript

--- deploy.cmd
+++ deploy.cmd
@@ -110,7 +110,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
 :: 4. Bundle TypeScript
 IF EXIST "%DEPLOYMENT_TARGET%\webpack.config.js" (
   pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NPM_CMD! run build
+  call webpack -p
   if !ERRORLEVEL! NEQ 0 goto error
   popd
 )
