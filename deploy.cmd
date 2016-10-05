aacc5573aecf96378bfbc03ba51513bde8b8a373
Nilesh-Bhor <nilesh-bhor@github.com>
2016-10-05 17:19:35 +0530

    changes related to azure deployment

--- deploy.cmd
+++ deploy.cmd
@@ -106,14 +106,17 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
 )
 
 :: 4. Compile TypeScript
-echo Transpiling TypeScript in %DEPLOYMENT_TARGET%...
-call :ExecuteCmd node %DEPLOYMENT_TARGET%\node_modules\typescript\bin\tsc -p "%DEPLOYMENT_TARGET%"
-
+IF EXIST "%DEPLOYMENT_TARGET%\webpack.config.js"(
+  pushd "%DEPLOYMENT_TARGET%"
+  echo "Building web site using webpack"
+  call :ExecuteCmd !NPM_CMD! build
+  if !ERRORLEVEL! NEQ 0 goto error
+  popd
+)
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 :: Post deployment stub
 IF DEFINED POST_DEPLOYMENT_ACTION call "%POST_DEPLOYMENT_ACTION%"
-call :ExecuteCmd !NPM_CMD! build
 IF !ERRORLEVEL! NEQ 0 goto error
 
 goto end
