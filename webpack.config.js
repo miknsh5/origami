2fc9f15edb8de911923df8c7773135535004cc98
Nilesh-Bhor <Nilesh-Bhor@github.com>
2017-01-04 15:35:10 +0530

    updated tsloader

--- webpack.config.js
+++ webpack.config.js
@@ -17,18 +17,20 @@ var config = {
         sourceMapFilename: '[name].map',
         chunkFilename: '[id].chunk.js'
     },
+    resolve: {
+        extensions: ['', '.ts', '.tsx', '.js', '.jsx', '.png', '.gif']
+    },
     module: {
         preLoaders: [
             { test: /\.js$/, loader: 'source-map-loader', exclude: /node_modules/ }
         ],
         loaders: [
-            { test: /\.ts$/, loader: 'awesome-typescript-loader' },
+            { test: /\.ts$/, loader: 'ts-loader' },
             { test: /\.json$/, loader: 'json-loader' },
             { test: /\.html/, loader: 'raw-loader' },
             { test: /\.css$/, loader: 'to-string-loader!css-loader' },
         ]
     },
-
     plugins: [
         new webpack.optimize.OccurenceOrderPlugin(true),
         new webpack.optimize.CommonsChunkPlugin({ name: ['polyfills', 'vendor', 'main'].reverse(), minChunks: Infinity }),
@@ -47,24 +49,10 @@ var config = {
             },
         ], { ignore: ['*.ts'], })
     ],
-    resolve: {
-        extensions: ['', '.ts', '.js', '.png', '.gif']
-    },
     devServer: {
         historyApiFallback: true,
         outputPath: path.join(__dirname, 'dist'),
         watchOptions: { aggregateTimeout: 300, poll: 1000 }
-    },
-    node: {
-        global: true,
-        process: true,
-        Buffer: false,
-        crypto: 'empty',
-        module: false,
-        clearImmediate: false,
-        setImmediate: false,
-        clearTimeout: true,
-        setTimeout: true
     }
 };
 module.exports = config;
