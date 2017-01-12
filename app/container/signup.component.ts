33375270fc5214e4f7495ac4617b670808d13bec
Nilesh-Bhor <Nilesh-Bhor@github.com>
2017-01-12 16:37:25 +0530

    fixed broken continous integration issue

--- app/container/signup.component.ts
+++ app/container/signup.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from "@angular/core";
 
 import { Router } from "@angular/router";
 
-import { AuthService } from "../login/index";
+import { AuthService } from "../shared/index";
 
 @Component({
     selector: "signup",
