67bfeba329399f9c44023fb6f1493e33c3666080
Nilesh-Bhor <Nilesh-Bhor@github.com>
2017-01-16 13:38:53 +0530

    updated deploy.cmd file

--- deploy.cmd
+++ deploy.cmd
@@ -100,7 +100,7 @@ call :SelectNodeVersion
 :: 3. Install npm packages
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NPM_CMD! install typings -g  
+  call :ExecuteCmd !NPM_CMD! install webpack -g
   call :ExecuteCmd !NPM_CMD! install --production
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
@@ -109,7 +109,6 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
 :: 4. Compile TypeScript
 echo Transpiling TypeScript in %DEPLOYMENT_TARGET%...
 pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NPM_CMD! install webpack -g
   call :ExecuteCmd webpack -p 
   IF !ERRORLEVEL! NEQ 0 goto error
 popd
