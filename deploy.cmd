1ac0c458f2a64f57af3c68bf512b1da699779ac6
anshulee <anshulee@cennest.com>
2017-01-16 13:17:25 +0530

    reverted deploy.cmd file changes

--- deploy.cmd
+++ deploy.cmd
@@ -100,7 +100,6 @@ call :SelectNodeVersion
 :: 3. Install npm packages
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NPM_CMD! install typings -g
   call :ExecuteCmd !NPM_CMD! install webpack -g
   call :ExecuteCmd !NPM_CMD! install --production
   IF !ERRORLEVEL! NEQ 0 goto error
